5953a9d0f87a1624dde20badd3b7246380ae79e2
cupidblack <eltkabymh@gmail.com>
2025-03-06 14:03:55 +0000

    Initialization

---
+++ wp-content/plugins/miniorange-oauth-20-server/admin/helper/constants/class-miniorange-oauth-20-server-oauth-constants.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class that defines various constants used through out the plugin.
+ *
+ * @package Miniorange_Oauth_20_Server/admin/helper/constants
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * This class defines various constants used through
+ * out the plugin.
+ */
+final class Miniorange_Oauth_20_Server_Oauth_Constants {
+	const QUERY_SUBMITTED    = 'Query submitted.';
+	const DENY_AUTHORIZATION = 'Authorization was denied by the user.';
+	const PRICING_PLAN_URL   = 'https://plugins.miniorange.com/wp-oauth-server#pricing';
+}
